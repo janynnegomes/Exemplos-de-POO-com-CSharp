8b015c542e40be96d25e2dcb8f387bf9d0f6caae
Janynne Gomes <janynnegomes@gmail.com>
2015-03-10 20:00:47 -0300

    Adição dos componentes
    Foram adicionados os componentes do computador ao método construtor.
    Dessa forma, ao criar um objeto dessa classe, os componenets já vem
    criados também.

--- Heranca/Herança/Herança/Computador.cs
+++ Heranca/Herança/Herança/Computador.cs
@@ -19,6 +19,20 @@ namespace Heranca
         public Computador(string _identificacao)
         {
             this.identificacao = _identificacao;
+
+            // Inicializa a variável
+            this.componentes = new List<string>();
+
+            // Adiciona por padrão alguns componentes básicos
+            this.componentes.Add("RAM 1GB");
+            this.componentes.Add("HD SATA 1TB");
+            this.componentes.Add("HD Serial 200GB");
+            this.componentes.Add("Placa Mãe Foxconn");
+            this.componentes.Add("Processador Core 2 Duo");
+            this.componentes.Add("Fonte");
+            this.componentes.Add("Mouse USB");
+            this.componentes.Add("Teclado USB");
+            this.componentes.Add("Monitor LED 23'");
         }
 
         public void Formatar() 

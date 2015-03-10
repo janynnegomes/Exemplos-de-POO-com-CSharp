367c49c0e3e8ea9c37b5ab8a6b6c310642da5b18
Janynne Gomes <janynnegomes@gmail.com>
2015-03-10 19:45:47 -0300

    Criação da classe Computador
    Foram definidas as carcaterísticas e comportamento da classe.

---
+++ Heranca/Herança/Herança/Computador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    // Nome da classe
+    class Computador
+    {
+        // Armazena a lista de componentes que o computador possui
+        List<string> componentes;
+
+        string fabricante;
+
+        public void Formatar() 
+        {
+            // Aqui você implementa seu código
+
+        }
+
+        public void Ligar() {
+            
+            Console.WriteLine("Computador ligado...");
+        }
+
+        public void Desligar() {
+
+            Console.WriteLine("Computador desligado...");
+        }
+
+        public void Reiniciar() {
+
+            this.Desligar();
+            this.Ligar();
+            
+        }
+    }
+}
--- Heranca/Herança/Herança/Program.cs
+++ Heranca/Herança/Herança/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Herança
+namespace Heranca
 {
     class Program
     {

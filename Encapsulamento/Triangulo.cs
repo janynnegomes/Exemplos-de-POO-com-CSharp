efd6725624a7e7f682d57f5a4a7dedf9aeabf629
Janynne Gomes <janynnegomes@gmail.com>
2015-03-16 19:52:27 -0300

    Adição do projeto ENcapsulamento e correção do path dos projetos

---
+++ Encapsulamento/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    class Triangulo
+    {
+
+        public float Lado_A { get; set; }
+        public float Lado_B { get; set; }
+        public float Lado_C { get; set; }
+        public string Cor { get; set; }
+
+    }
+}
